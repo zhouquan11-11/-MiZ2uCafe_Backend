dbc23ca115e3e5f318e0e2eae8f62b6a113d3f0d
zhouquan11 <B2400458@helplive.edu.my>
2025-04-24 12:19:50 +0800

    Update db_connect.php

--- MiZ2uCafe_Backend/db_connect.php
+++ MiZ2uCafe_Backend/db_connect.php
@@ -1,15 +1,20 @@
 <?php
 $host = 'localhost';
-$db   = 'miz2u_cafe';
-$user = 'root';
-$pass = '';
+$db   = 'miz2ucafe';
+$user = 'your_username';
+$pass = 'your_password';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
 
 try {
-    $pdo = new PDO($dsn, $user, $pass);
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
